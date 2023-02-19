edb1fa076e17b92e42b4d662e692de43b003da3b
MG2022 <10505124+MartinGue@users.noreply.github.com>
2023-02-19 15:45:25 +0100

    H3 Novice_to_ninja logic

--- JavaScript/H1 JS from novice to ninja/quiz_ninja/H3 Logic/for_loops.js
+++
@@ -1,8 +0,0 @@
-var bottles = 10; // bottles is initialized here instead
-
-for ( ; bottles > 0 ; ) { // empty initialization and increment
-
-alert("There were " + bottles + " green bottles, hanging on the wall. And if one green bottle should accidently fall, there'd be " 
-+ (bottles-1) + " green bottles hanging on the wall");
-bottles--; // increment moved into code block
-}
